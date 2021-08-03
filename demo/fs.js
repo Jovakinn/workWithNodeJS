ab3413651816d26296ae09a076a51b12db8883f2
Jovakinn <max05012004@gmail.com>
2021-08-03 21:52:28 +0300

    Initial commit

---
+++ demo/fs.js
@@ -0,0 +1,35 @@
+// File System
+const fs = require('fs')
+const path = require('path')
+
+/*
+fs.mkdir(path.join(__dirname, 'test'), (err) =>{
+    if (err) {
+        throw err;
+    }
+    console.log('Folder was created')
+})*/
+
+const filePath = path.join(__dirname, 'test', 'text.txt')
+/*fs.writeFile(filePath, 'Hello NodeJS!', (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('File was created')
+
+    fs.appendFile(filePath, '\nHello again', (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('File was created')
+    })
+})*/
+fs.readFile(filePath, 'utf-8' ,(err, content) =>{
+    if (err){
+        throw err;
+    }
+
+    console.log(content)
+   /* const data = Buffer.from(content)
+    console.log('Content: ', data.toString())*/
+})
