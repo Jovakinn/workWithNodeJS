14229b1106ec53ab820588e02882ce1a5f667662
Jovakinn <max05012004@gmail.com>
2021-09-22 14:18:04 +0300

    add routes and additional interface

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+const PORT = process.env.PORT || 3000
+
+const app = express()
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
@@ -14,27 +16,19 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(todoRoutes)
-
-const helloResponse = app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-const contactResponse = app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + "/public/contact.html");
-});
-
-if (app.get !== helloResponse && app.get !== contactResponse){
-    app.get((req, res) => {
-        res.sendFile(__dirname + "/public/error.html");
-    });
-}
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://jovakinn:1q2w3e4r@cluster0.endkw.mongodb.net/todos', {
-            useNewUrlParser: true,
-            useFindAndModify: false
-        })
+        await mongoose.connect(
+            'mongodb+srv://jovakinn:1q2w3e4r@cluster0-ua4e7.mongodb.net/todos',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: false
+            }
+        )
         app.listen(PORT, () => {
             console.log('Server has been started...')
         })
@@ -42,4 +36,5 @@ async function start() {
         console.log(e)
     }
 }
+
 start()
